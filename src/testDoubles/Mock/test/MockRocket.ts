a91130e0da8afe9440b8fb496057e7e5c8cf6226
to-kun <to_iwahana@mail.toyota.co.jp>
2026-09-07 13:12:17 +0900

    answer/mockブランチ作成

--- src/testDoubles/Mock/test/MockRocket.ts
+++ src/testDoubles/Mock/test/MockRocket.ts
@@ -1,21 +1,28 @@
 import type {Rocket} from "../Types.js";
+import { expect } from "vitest";
 
 export class MockRocket implements Rocket {
     private fire_wasCalled = false
     fire(): void {
         // リファクタリング後のテストが通るように実装してください
+        this.fire_wasCalled = true
     }
 
     private abort_wasCalled = false
     abort(): void {
         // リファクタリング後のテストが通るように実装してください
+        this.abort_wasCalled = true
     }
 
     verifyTrigger() {
         // リファクタリング後のテストが通るように実装してください
+        expect(this.fire_wasCalled).toBe(true)
+        expect(this.abort_wasCalled).toBe(false)
     }
 
     verifyAbort() {
         // リファクタリング後のテストが通るように実装してください
+        expect(this.fire_wasCalled).toBe(false)
+        expect(this.abort_wasCalled).toBe(true)
     }
 }
